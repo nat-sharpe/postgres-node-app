ca372954eaff39e1075fc1179fd0dd4a356c2107
Nat Sharpe <sharpe1890@gmail.com>
2018-08-25 21:16:18 -0400

    fixed bug in high-low sorter. Now works properly compressing and decopressing

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-const pg = require('pg-promise')();
+let str = 'What does the fox say?';
+let strArray = str.split('');
+let frequency = {};
+
+strArray.forEach(letter => {
+    if (letter in frequency) {
+        frequency[letter] = frequency[letter] + 1;
+    } else {
+        frequency[letter] = 1;
+    }
+});
+
+console.log(frequency);
+
+let allKeys = Object.keys(frequency); console.log(allKeys)
+let allValues = Object.values(frequency); console.log(allValues)
+let sortedCharsArray = [];
+let sortedChars = [];
+let maxIndex;
+
+for (let ii = 0; ii < Object.keys(frequency).length; ii++) {
+    let max = Math.max(...allValues);
+    for (let jj = 0; jj < Object.keys(frequency).length; jj++) {
+        if (allValues[jj] == max && jj !== maxIndex) {
+            maxIndex = jj;
+            allValues[jj] = 0;
+            break;
+        }
+    }
+    sortedChars.push(allKeys[maxIndex]);    
+};
+
+console.log(sortedChars);
+
+let formula = {};
+let first = false;
+let zeroes = '0';
+
+sortedChars.forEach((letter, index) => {
+    // if (index == sortedChars.length - 1) {
+    //     formula[letter] = zeroes;
+    // }
+    if (first) {
+        formula[letter] = zeroes + 1;
+        zeroes = zeroes + '0';
+    } else {
+        first = true;
+        formula[letter] = '1';
+    }
+});
+
+console.log(formula);
+
+let compressed = '';
+
+strArray.forEach((letter) => {
+    compressed = compressed + formula[letter];
+});
+
+
+console.log(compressed)
+
+let decrypt = compressed.split('1'); console.log(decrypt)
+let newString = '';
+let formulaValues = Object.values(formula); console.log(formulaValues)
+let formulaKeys = Object.keys(formula); console.log(formulaKeys)
+
+decrypt.forEach((char, index) => {
+    if (index < (decrypt.length -1)) {
+        char = char + '1'; 
+        index = formulaValues.indexOf(char); 
+        newString += formulaKeys[index];
+    }
+});
+
+console.log(str);
+console.log(compressed);
+console.log(newString);
