7d5d996408cde6b60d9dd74318df19d4e0c654c8
Nat Sharpe <sharpe1890@gmail.com>
2018-08-25 22:07:23 -0400

    Broke code down  into pure functions

--- index.js
+++ index.js
@@ -1,78 +1,91 @@
-let str = 'What does the fox say?';
-let strArray = str.split('');
-let frequency = {};
 
-strArray.forEach(letter => {
-    if (letter in frequency) {
-        frequency[letter] = frequency[letter] + 1;
-    } else {
-        frequency[letter] = 1;
-    }
-});
+let getFormula = (sortedChars) => {
+    let formula = {};
+    let first = false;
+    let zeroes = '0';
 
-console.log(frequency);
-
-let allKeys = Object.keys(frequency); console.log(allKeys)
-let allValues = Object.values(frequency); console.log(allValues)
-let sortedCharsArray = [];
-let sortedChars = [];
-let maxIndex;
-
-for (let ii = 0; ii < Object.keys(frequency).length; ii++) {
-    let max = Math.max(...allValues);
-    for (let jj = 0; jj < Object.keys(frequency).length; jj++) {
-        if (allValues[jj] == max && jj !== maxIndex) {
-            maxIndex = jj;
-            allValues[jj] = 0;
-            break;
+    sortedChars.forEach((letter) => {
+        if (first) {
+            formula[letter] = zeroes + 1;
+            zeroes = zeroes + '0';
+        } else {
+            first = true;
+            formula[letter] = '1';
         }
-    }
-    sortedChars.push(allKeys[maxIndex]);    
+    });
+    return formula;
 };
 
-console.log(sortedChars);
+let sortHighLow = (frequency) => {
+    let allKeys = Object.keys(frequency); 
+    let allValues = Object.values(frequency);
+    let sortedChars = [];
+    let maxIndex;
 
-let formula = {};
-let first = false;
-let zeroes = '0';
+    for (let ii = 0; ii < Object.keys(frequency).length; ii++) {
+        let max = Math.max(...allValues);
+        for (let jj = 0; jj < Object.keys(frequency).length; jj++) {
+            if (allValues[jj] == max && jj !== maxIndex) {
+                maxIndex = jj;
+                allValues[jj] = 0;
+                break;
+            }
+        }
+        sortedChars.push(allKeys[maxIndex]);    
+    };
+    return getFormula(sortedChars);
+};
 
-sortedChars.forEach((letter, index) => {
-    // if (index == sortedChars.length - 1) {
-    //     formula[letter] = zeroes;
-    // }
-    if (first) {
-        formula[letter] = zeroes + 1;
-        zeroes = zeroes + '0';
-    } else {
-        first = true;
-        formula[letter] = '1';
-    }
-});
+let findFrequency = (strArray) => {
+    let frequency = {};
+    strArray.forEach(letter => {
+        if (letter in frequency) {
+            frequency[letter] = frequency[letter] + 1;
+        } else {
+            frequency[letter] = 1;
+        }
+    });
+    return sortHighLow(frequency);
+};
 
-console.log(formula);
+let writeBody = (strArray, formula) => {
+    let compressed = '';
+    strArray.forEach((letter) => {
+        compressed = compressed + formula[letter];
+    });
+    return compressed;
+};
 
-let compressed = '';
+let compress = (str) => {
+    let strArray = str.split('');
+    let formula = findFrequency(strArray);
+    let body = writeBody(strArray, formula);
+    return {body, formula};
+};
 
-strArray.forEach((letter) => {
-    compressed = compressed + formula[letter];
-});
+let decompress = (body, formula) => {
+    let strArray = body.split('1'); 
+    let formulaValues = Object.values(formula); 
+    let formulaKeys = Object.keys(formula);
+    let decompressed = '';
+    
+    strArray.forEach((char, index) => {
+        if (index < (strArray.length -1)) {
+            index = formulaValues.indexOf(char + '1'); 
+            decompressed += formulaKeys[index];
+        }
+    });
 
+    return decompressed;
+};
 
-console.log(compressed)
+let test = (str) => {
+    let compressed = compress(str);
+    let decompressed = decompress(compressed.body, compressed.formula)
+    console.log(compressed.body);
+    console.log(compressed.formula);
+    console.log(decompressed);
+};
 
-let decrypt = compressed.split('1'); console.log(decrypt)
-let newString = '';
-let formulaValues = Object.values(formula); console.log(formulaValues)
-let formulaKeys = Object.keys(formula); console.log(formulaKeys)
-
-decrypt.forEach((char, index) => {
-    if (index < (decrypt.length -1)) {
-        char = char + '1'; 
-        index = formulaValues.indexOf(char); 
-        newString += formulaKeys[index];
-    }
-});
-
-console.log(str);
-console.log(compressed);
-console.log(newString);
+let message = 'The wind in the willows...'
+test(message);
